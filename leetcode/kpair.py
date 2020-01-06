e20dfbef77b83e7224698b63e9ba9197f6e65e4b
Advaith Doosa <advaith@chainist.com>
2020-01-06 19:26:43 +0530

    started leetcode

---
+++ leetcode/kpair.py
@@ -0,0 +1,17 @@
+import collections
+
+
+def findKPairs(im junums, k):
+    res = 0
+    c = collections.Counter(nums)
+    for i in c:
+        if k > 0 and i + k in c or k == 0 and c[i] > 1:
+            res += 1
+    return res
+
+
+test1 = [3, 1, 4, 1, 5]
+print('Running test case one')
+findKPairs(test1, 2)
+# Input: [3, 1, 4, 1, 5], k = 2
+# Output is 2
