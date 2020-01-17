793e37b1ef6fe7c020692727862f615b91bfc8b6
Advaith Doosa <advaith@chainist.com>
2020-01-17 19:09:42 +0530

    test passing + removed unnecessary code

--- CTCI-Solutions/CH10-SortingSearching/MergeTwoArrays.py
+++ CTCI-Solutions/CH10-SortingSearching/MergeTwoArrays.py
@@ -8,36 +8,29 @@
 import unittest
 
 
-def mergeArrays(A, B):
+def mergeSortedArrays(A, B):
     index = len(A) - 1
-    indexA = len(A) - len(B) - 1
     indexB = len(B) - 1
-
+    indexA = len(A) - len(B) - 1
     while indexB >= 0:
-        if indexA > 0 and A[indexA] > B[indexB]:
+        if indexA >= 0 and A[indexA] > B[indexB]:
             A[index] = A[indexA]
-            index -= 1
             indexA -= 1
         else:
             A[index] = B[indexB]
-            index -= 1
             indexB -= 1
+        index -= 1
+
     return A
 
-
-a1 = [33, 44, 55, 66, 88, 99, None, None, None]
-b1 = [11, 22, 77]
-
-# res = mergeArrays(a1, b1)
-
-# print(res)
+# Test
 
 
 class Test(unittest.TestCase):
     def testSortedMerge(self):
         a = [33, 44, 55, 66, 88, 99, None, None, None]
         b = [11, 22, 77]
-        res = mergeArrays(a, b)
+        res = mergeSortedArrays(a, b)
         self.assertEqual(res, [11, 22, 33, 44, 55, 66, 77, 88, 99])
 
 
